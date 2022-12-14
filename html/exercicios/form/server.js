1881f02a60716c03a54ccadecfbb01bdbbf156c9
Matheus <jinkamuya@gmail.com>
2022-12-14 11:31:58 -0300

    adição do formulário e também da aba de imagens

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -5,7 +5,13 @@ app.use(express.urlencoded())
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body);
-    resp.send('<h1>Parabéns</h1>');
+    resp.send('<h1>Parabéns, usuário incluido!!!</h1>');
 })
 
-app.listen(8080)
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body);
+    resp.send('<h1>Parabéns, usuário alterado!!!</h1>');
+})
+
+app.listen(5500)
